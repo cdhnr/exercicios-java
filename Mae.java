378f4d4cf54f165221c198af2db659ff1927dbfb
Henrique <cdhnr@icloud.com>
2025-06-17 21:35:20 -0300

    Add files via upload

---
+++ Mae.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// Exemplos de repetições com "while".
+
+public class Mae{
+    public static void main (String[] args){
+        int mae = 1;
+        while (mae<=5) {
+            System.out.println("Quantas vezes eu vou ter que te falar, filho? " + mae + "° vez");
+            mae++;
+        }
+    System.out.println("Entendi, mãe.");
+    }
+}
